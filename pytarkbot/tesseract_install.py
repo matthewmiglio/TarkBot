3675af31a845b5f2050d71208362ad82c1132d63
Martin Miglio <marmig0404@gmail.com>
2022-08-14 18:35:57 -0400

    add docstrings

--- pytarkbot/tesseract_install.py
+++ pytarkbot/tesseract_install.py
@@ -15,6 +15,12 @@ install_info_file = join(top_level, 'tesseract_install.json')
 
 
 class DownloadProgressBar(tqdm):
+    """Progress bar for downloasd
+
+    Args:
+        tqdm (class): tqdm class
+    """
+
     def update_to(self, b=1, bsize=1, tsize=None) -> None:
         if tsize is not None:
             self.total = tsize
@@ -22,6 +28,19 @@ class DownloadProgressBar(tqdm):
 
 
 def download_from_url(url: str, output_dir: str, file_name: str) -> str | None:
+    """Downloads a file from a url to a directory with a specified file name
+
+    Args:
+        url (str): url of item to download
+        output_dir (str): directory to save file
+        file_name (str): name of file to save
+
+    Raises:
+        ValueError: thrown when url does not specify an http or https endpoint
+
+    Returns:
+        str | None: returns the path to the file or None if not downloaded
+    """
     if not url.lower().startswith('http'):
         raise ValueError from None
     if not exists(join(output_dir, file_name)):
@@ -47,6 +66,11 @@ def download_from_url(url: str, output_dir: str, file_name: str) -> str | None:
 
 
 def make_cache() -> str:
+    """creates a cache directory for downloaded installer
+
+    Returns:
+        str: the path to the cache directory
+    """
     cache_dir = join(dirname(__file__), 'cache')
     if not exists(cache_dir):
         makedirs(cache_dir)
@@ -54,6 +78,11 @@ def make_cache() -> str:
 
 
 def get_tsrct_link() -> list[str] | None:
+    """retrieves the link to the latest tesseract download
+
+    Returns:
+        list[str] | None: returns a a list of strings as [url, name of file] or None if not found
+    """
     url = r"http://digi.bib.uni-mannheim.de/tesseract/"
     ver_df = read_html(url)[0]
     latest_ver = ver_df[
@@ -64,10 +93,23 @@ def get_tsrct_link() -> list[str] | None:
 
 
 def run_installer(path: str) -> bool:
+    """runs a specified installer executable
+
+    Args:
+        path (str): path to installer executable
+
+    Returns:
+        bool: if install was successful
+    """
     return 0 == call(path, shell=False)
 
 
 def install_dependencies(dependencies: dict[str, list[str] | None]) -> None:
+    """installs a list of dependencies from a supplied dictionary
+
+    Args:
+        dependencies (dict[str, list[str]  |  None]): a dictioinary of dependencies names and their url
+    """
     cache_dir = make_cache()
     for name, download_info in dependencies.items():
         if download_info is not None:
@@ -83,6 +125,11 @@ def install_dependencies(dependencies: dict[str, list[str] | None]) -> None:
 
 
 def get_tsrct_path() -> str:
+    """locate the path of the tesseract directory
+
+    Returns:
+        str: the path of the tesseract directory
+    """
     try:
         akey = r"SOFTWARE\Tesseract-OCR"
         areg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
@@ -95,6 +142,8 @@ def get_tsrct_path() -> str:
 
 
 def install_tesseract() -> None:
+    """install the latest version of tesseract
+    """
     dependency_dict = {
         "tesseract": get_tsrct_link()
     }
@@ -102,6 +151,8 @@ def install_tesseract() -> None:
 
 
 def setup_tesseract() -> None:
+    """define the environmental variables related to tesseract, install tesseract if neccessary
+    """
     try:
         tesseract_path = get_tsrct_path()
     except FileNotFoundError:
