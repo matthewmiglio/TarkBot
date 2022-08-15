eb8d801584ba3fc998c1154dd1bcbb00b753e8ec
Matthew <matmigg0804@gmail.com>
2022-08-14 23:31:28 -0700

    fixing check_if_on_tarmain()

--- debug.py
+++ debug.py
@@ -1,9 +1,16 @@
-from pytarkbot.client import orientate_tarkov_client, resize_window
+
+from pytarkbot.client import resize_window
+from pytarkbot.hideout import get_to_hideout
+from pytarkbot.launcher import check_if_on_tark_main
 from pytarkbot.logger import Logger
 
 logger=Logger()
 
 title="EscapeFromTarkov"
-resize=[1280,960]
+resize=[1299,999]
 
-resize_window(window_name=title,resize=resize)
+# resize_window(window_name=title,resize=resize)
+
+print(check_if_on_tark_main(logger))
+
+get_to_hideout(logger)
