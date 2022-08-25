f06bdb638d0a362512fff0d60049ee4429e4ec79
Matthew Miglio <matmigg0804@gmail.com>
2022-08-25 15:24:35 -0400

    making orientate_filters_window more robust

--- pytarkbot/flee.py
+++ pytarkbot/flee.py
@@ -674,6 +674,22 @@ def find_filters_window():
         "8.png",
         "9.png",
         "10.png",
+        "11.png",
+        "12.png",
+        "13.png",
+        "14.png",
+        "15.png",
+        "16.png",
+        "17.png",
+        "18.png",
+        "19.png",
+        "20.png",
+        "21.png",
+        "22.png",
+        
+        
+        
+        
     ]
 
     locations = find_references(
