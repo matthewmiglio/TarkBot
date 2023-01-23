df51dde95bce2dd8ce048c38517845b9beb3d721
Martin Miglio <marmig0404@gmail.com>
2023-01-23 16:06:17 -0500

    search registry for bsglauncher install location first

--- pytarkbot/utils/dependency.py
+++ pytarkbot/utils/dependency.py
@@ -6,21 +6,37 @@ from winreg import HKEY_LOCAL_MACHINE, ConnectRegistry, OpenKey, QueryValueEx
 def get_bsg_launcher_path() -> str:
     """get the path to the bsg launcher"""
     try:
-        akey = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"
+        akey = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{B0FDA062-7581-4D67-B085-C4E7C358037F}_is1"
         areg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
         akey = OpenKey(areg, akey)
     except FileNotFoundError:
-        akey = r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"
+        akey = r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{B0FDA062-7581-4D67-B085-C4E7C358037F}_is1"
         areg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
         akey = OpenKey(areg, akey)
     launcher_file = abspath(
-        join(
-            QueryValueEx(akey, "InstallLocation")[0],
-            pardir,
-            "BsgLauncher",
-            "BsgLauncher.exe",
-        )
+        join(QueryValueEx(akey, "InstallLocation")[0], "BsgLauncher.exe")
     )
     if not Path(launcher_file).exists():
-        raise FileNotFoundError("Launcher not found")
+        try:
+            akey = (
+                r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"
+            )
+            areg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+            akey = OpenKey(areg, akey)
+        except FileNotFoundError:
+            akey = r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"
+            areg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+            akey = OpenKey(areg, akey)
+        launcher_file = abspath(
+            join(
+                QueryValueEx(akey, "InstallLocation")[0],
+                pardir,
+                "BsgLauncher",
+                "BsgLauncher.exe",
+            )
+        )
     return launcher_file
+
+
+if __name__ == "__main__":
+    print(get_bsg_launcher_path())
