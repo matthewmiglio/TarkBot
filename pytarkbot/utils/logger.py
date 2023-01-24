e49359aabcf3f47e075223b331059943be761464
Martin Miglio <marmig0404@gmail.com>
2023-01-24 00:22:22 -0500

    update queue with all stats to fix GUI not reading values for offers_removed, starting_money, current_money, or fee_total from logger  #18

--- pytarkbot/utils/logger.py
+++ pytarkbot/utils/logger.py
@@ -35,9 +35,15 @@ class Logger:
             "time_since_start": self.calc_time_since_start(),
             "restarts": self.restarts,
             "item_sold": self.item_sold,
-            "roubles_made": self.format_roubles_made(),
+            "roubles_made": self.format_money(
+                self.roubles_made
+            ),  # self.format_money(self.current_money - self.starting_money), # might be better
             "sale_attempts": self.sale_attempts,
             "success_rate": self.calc_success_rate(),
+            "offers_removed": self.offers_removed,
+            "fee_total": self.format_money(self.fee_total),
+            "starting_money": self.format_money(self.starting_money),
+            "current_money": self.format_money(self.current_money),
         }
         self.queue.put(statistics)
 
@@ -102,12 +108,13 @@ class Logger:
         else:
             self.fee_total = (self.starting_money) - (self.current_money)
 
-    def format_roubles_made(self) -> str:
-        if self.roubles_made < 1000:
-            return str(self.roubles_made)
-        if self.roubles_made < 1000000:
-            return f"{(self.roubles_made / 1000):.0f}k"
-        return f"{(self.roubles_made / 1000000):.2f}m"
+    @staticmethod
+    def format_money(money: int) -> str:
+        if money < 1000:
+            return str(money)
+        if money < 1000000:
+            return f"{(money / 1000):.0f}k"
+        return f"{(money / 1000000):.2f}m"
 
     def calc_success_rate(self) -> str:
         if self.sale_attempts == 0 or self.item_sold == 0:
