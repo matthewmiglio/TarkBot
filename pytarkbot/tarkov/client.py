69d64a5a544721c375fa6c14738605396ee91252
Matt Main_PC <matmigg0804@gmail.com>
2023-03-23 15:27:33 -0400

    changing orientate_terminal()

--- pytarkbot/tarkov/client.py
+++ pytarkbot/tarkov/client.py
@@ -24,7 +24,7 @@ def orientate_launcher():
 def orientate_terminal():
     try:
         terminal_window = pygetwindow.getWindowsWithTitle("Py-TarkBot")[0]
-        terminal_window.moveTo(1292, 0)
+        terminal_window.moveTo(pyautogui.size()[0] - 430, 0)
     except:
         print("Couldnt orientate terminal.")
 
