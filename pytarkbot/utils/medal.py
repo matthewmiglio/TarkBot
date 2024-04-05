66dcd39439e7dd892891178f8aab1248d498477a
Martin Miglio <marmig0404@gmail.com>
2024-04-05 18:04:09 -0400

    restore medal at exit

--- pytarkbot/utils/medal.py
+++ pytarkbot/utils/medal.py
@@ -1,4 +1,7 @@
+import os
 import psutil
+import atexit
+
 
 # method to get a list of program names, and their PIDs
 def get_programs():
@@ -52,6 +55,40 @@ def close_all_medal():
     for pid in pids:
         close_program(pid)
     print(f"Closed {len(pids)} medal instances!")
+
+
+def get_medal_base_path():
+    # using registrey to get the path:
+    # ntuser > root > software > microsoft > windows > currentversion > uninstall > medal
+
+    # get the path from the registry:
+    import winreg
+
+    # open the registry key
+    key = winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\CurrentVersion\Uninstall\Medal",
+    )
+
+    # get the value of the key
+    path = winreg.QueryValueEx(key, "InstallLocation")[0]
+
+    return path
+
+
+@atexit.register
+def restore_medal():
+    # get the base path
+    try:
+        base_path = get_medal_base_path()
+
+        # get the exe path
+        exe_path = os.path.join(base_path, "Medal.exe")
+
+        # run the exe
+        os.system(f"start {exe_path}")
+    except:
+        pass
 
 
 close_all_medal()
